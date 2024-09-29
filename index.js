7ba7414dba5efe1482a97f21b0daae05d0018dfb
jdszekeres <jdszekeres@gmail.com>
2024-09-29 13:31:48 -0700

    add image caching and reformated submit button

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 const request = require('request');
+const NodeFetchCache = require('node-fetch-cache').NodeFetchCache;
+const FileSystemCache = require('node-fetch-cache').FileSystemCache;
+
+
+const fetch = NodeFetchCache.create({
+  cache: new FileSystemCache({ cacheDirectory: 'images_cache' }),
+});
 
 const app = express()
 const port = 3000
@@ -44,61 +51,61 @@ const places = [
   { name: "Arches National Park, UT", coord: [38.743517, -109.499319], zoom: 20 },
   { name: "Tyndale Glacier, CO", coord: [40.305308, -105.688856], zoom: 15 },
   { name: "Pikes Peak, CO", coord: [38.843457, -105.041110], zoom: 10 },
-  {name: "Aspen, CO", coord: [39.192528, -106.870659], zoom: 13 },
-  {name: "Grand Teton National Park, WY", coord: [43.740914, -110.802760], zoom: 10 },
-  {name: "Devil's Tower, WY", coord: [44.590492, -104.715150], zoom: 17 },
-  {name: "Badlands National Monument", coord: [43.674988, -102.412062], zoom: 6},
-  {name: "Lake Itasca, MN", coord: [47.218231, -95.205295], zoom: 12},
-  {name: "Presque Isle, MI", coord: [45.301598, -83.479333], zoom: 12},
-  {name: "Mackincac, MI", coord: [45.816621, -84.727945], zoom: 11},
-  {name: "Washburn, WI", coord: [46.669776, -90.890719], zoom: 11},
-  {name: "United States Steel Mill, OH", coord: [41.451825, -82.124736], zoom: 15},
-  {name: "Gateway Arch, MO", coord: [38.624674, -90.184852], zoom: 17 },
-  {name: "Great Smokey Mountains National Park, TN", coord: [35.610656, -83.433273], zoom: 13},
-  {name: "Key West, FL", coord: [24.559191, -81.778234], zoom: 12},
-  {name: "Miami, FL", coord: [25.845167, -80.120413], zoom: 14},
-  {name: "Everglades, FL", coord: [25.262158, -80.981535], zoom: 10},
-  {name: "Williamsburg, VA", coord: [37.272295, -76.703638], zoom: 15},
-  {name: "Norfolk, VA", coord: [36.925957, -76.326584], zoom: 15},
-  {name: "Washington Monument, DC", coord: [38.889514, -77.035452], zoom: 17},
-  {name: "Niagra Falls, NY", coord: [43.077532, -79.074846], zoom: 16},
-  {name: "Cape Cod, Ma", coord: [41.806065, -69.939503], zoom: 13},
+  { name: "Aspen, CO", coord: [39.192528, -106.870659], zoom: 13 },
+  { name: "Grand Teton National Park, WY", coord: [43.740914, -110.802760], zoom: 10 },
+  { name: "Devil's Tower, WY", coord: [44.590492, -104.715150], zoom: 17 },
+  { name: "Badlands National Monument", coord: [43.674988, -102.412062], zoom: 6 },
+  { name: "Lake Itasca, MN", coord: [47.218231, -95.205295], zoom: 12 },
+  { name: "Presque Isle, MI", coord: [45.301598, -83.479333], zoom: 12 },
+  { name: "Mackincac, MI", coord: [45.816621, -84.727945], zoom: 11 },
+  { name: "Washburn, WI", coord: [46.669776, -90.890719], zoom: 11 },
+  { name: "United States Steel Mill, OH", coord: [41.451825, -82.124736], zoom: 15 },
+  { name: "Gateway Arch, MO", coord: [38.624674, -90.184852], zoom: 17 },
+  { name: "Great Smokey Mountains National Park, TN", coord: [35.610656, -83.433273], zoom: 13 },
+  { name: "Key West, FL", coord: [24.559191, -81.778234], zoom: 12 },
+  { name: "Miami, FL", coord: [25.845167, -80.120413], zoom: 14 },
+  { name: "Everglades, FL", coord: [25.262158, -80.981535], zoom: 10 },
+  { name: "Williamsburg, VA", coord: [37.272295, -76.703638], zoom: 15 },
+  { name: "Norfolk, VA", coord: [36.925957, -76.326584], zoom: 15 },
+  { name: "Washington Monument, DC", coord: [38.889514, -77.035452], zoom: 17 },
+  { name: "Niagra Falls, NY", coord: [43.077532, -79.074846], zoom: 16 },
+  { name: "Cape Cod, Ma", coord: [41.806065, -69.939503], zoom: 13 },
   //END places from Landscapes of America book
 
   //START airports
-  {name: "KDEN", coord: [39.856120, -104.676363], zoom: 11},
-  {name: "KIAD", coord: [38.951375, -77.456734], zoom: 11},
-  {name: "KORD", coord: [41.977164, -87.905138], zoom: 12},
-  {name: "KDFW", coord: [32.901422, -97.040242], zoom: 12},
-  {name: "KIAH", coord: [29.987071, -95.342269], zoom: 12},
-  {name: "KSFO", coord: [37.618685, -122.381466], zoom: 13},
-  {name: "KMCO", coord: [28.432498, -81.308039], zoom: 12},
-  {name: "KSLC", coord: [40.789598, -111.986876], zoom: 12},
-  {name: "KJFK", coord: [40.644491, -73.779644], zoom: 12},
-  {name: "KDTW", coord: [42.213021, -83.352824], zoom: 12},
-  {name: "KLAX", coord: [33.943980, -118.402944], zoom: 12},
-  {name: "KATL", coord: [33.640894, -84.429300], zoom: 13},
-  {name: "KLAS", coord: [36.084905, -115.151657], zoom: 12},
-  {name: "KCLT", coord: [35.220671, -80.949987], zoom: 13},
-  {name: "KMIA", coord: [25.793379, -80.280183], zoom: 12},
-  {name: "KSEA", coord: [47.449611, -122.306748], zoom: 12},
-  {name: "KEWR", coord: [40.690134, -74.176114], zoom: 13},
-  {name: "KPHX", coord: [33.434916, -112.011602], zoom: 13},
-  {name: "KBOS", coord: [42.366337, -71.010630], zoom: 12},
-  {name: "KFLL", coord: [26.071551, -80.147475], zoom: 13},
-  {name: "KMSP", coord: [44.884802, -93.214121], zoom: 13},
-  {name: "KLGA", coord: [40.779624, -73.874960], zoom: 13},
-  {name: "KPHL", coord: [39.874548, -75.242144], zoom: 12},
-  {name: "KBWI", coord: [39.179392, -76.667598], zoom: 13},
-  {name: "KDCA", coord: [38.850885, -77.040096], zoom: 13},
-  {name: "KSAN", coord: [32.733713, -117.190466], zoom: 13},
-  {name: "KTPA", coord: [27.976648, -82.530300], zoom: 12},
-  {name: "KBNA", coord: [36.123753, -86.677396], zoom: 13},
-  {name: "KAUS", coord: [30.196699, -97.666407], zoom: 13},
-  {name: "KMDW", coord: [41.785597, -87.752084], zoom: 13},
-  {name: "KHNL", coord: [21.315290, -157.926407], zoom: 13},
-  {name: "KANC", coord: [61.176267, -149.991052], zoom: 12},
-  {name: "KVCV", coord: [34.591923, -117.380446], zoom: 12}
+  { name: "KDEN", coord: [39.856120, -104.676363], zoom: 11 },
+  { name: "KIAD", coord: [38.951375, -77.456734], zoom: 11 },
+  { name: "KORD", coord: [41.977164, -87.905138], zoom: 12 },
+  { name: "KDFW", coord: [32.901422, -97.040242], zoom: 12 },
+  { name: "KIAH", coord: [29.987071, -95.342269], zoom: 12 },
+  { name: "KSFO", coord: [37.618685, -122.381466], zoom: 13 },
+  { name: "KMCO", coord: [28.432498, -81.308039], zoom: 12 },
+  { name: "KSLC", coord: [40.789598, -111.986876], zoom: 12 },
+  { name: "KJFK", coord: [40.644491, -73.779644], zoom: 12 },
+  { name: "KDTW", coord: [42.213021, -83.352824], zoom: 12 },
+  { name: "KLAX", coord: [33.943980, -118.402944], zoom: 12 },
+  { name: "KATL", coord: [33.640894, -84.429300], zoom: 13 },
+  { name: "KLAS", coord: [36.084905, -115.151657], zoom: 12 },
+  { name: "KCLT", coord: [35.220671, -80.949987], zoom: 13 },
+  { name: "KMIA", coord: [25.793379, -80.280183], zoom: 12 },
+  { name: "KSEA", coord: [47.449611, -122.306748], zoom: 12 },
+  { name: "KEWR", coord: [40.690134, -74.176114], zoom: 13 },
+  { name: "KPHX", coord: [33.434916, -112.011602], zoom: 13 },
+  { name: "KBOS", coord: [42.366337, -71.010630], zoom: 12 },
+  { name: "KFLL", coord: [26.071551, -80.147475], zoom: 13 },
+  { name: "KMSP", coord: [44.884802, -93.214121], zoom: 13 },
+  { name: "KLGA", coord: [40.779624, -73.874960], zoom: 13 },
+  { name: "KPHL", coord: [39.874548, -75.242144], zoom: 12 },
+  { name: "KBWI", coord: [39.179392, -76.667598], zoom: 13 },
+  { name: "KDCA", coord: [38.850885, -77.040096], zoom: 13 },
+  { name: "KSAN", coord: [32.733713, -117.190466], zoom: 13 },
+  { name: "KTPA", coord: [27.976648, -82.530300], zoom: 12 },
+  { name: "KBNA", coord: [36.123753, -86.677396], zoom: 13 },
+  { name: "KAUS", coord: [30.196699, -97.666407], zoom: 13 },
+  { name: "KMDW", coord: [41.785597, -87.752084], zoom: 13 },
+  { name: "KHNL", coord: [21.315290, -157.926407], zoom: 13 },
+  { name: "KANC", coord: [61.176267, -149.991052], zoom: 12 },
+  { name: "KVCV", coord: [34.591923, -117.380446], zoom: 12 }
   //END airports
 ]
 
@@ -115,7 +122,7 @@ app.get('/place', (req, res) => {
   const chosen = places[Math.floor(Math.random() * places.length)];
   // const chosen = places.at(-1);
   res.end(JSON.stringify(chosen));
-  
+
 })
 
 
@@ -133,17 +140,13 @@ app.get('/tile/:zoom/:x/:y', (req, res) => {
     if (resp.status === 200) {
       res.set("Content-Type", "image/jpeg");
 
-      resp.body.pipeTo(new WritableStream({
-        start() {
 
-        },
-        write(chunk) {
-          res.write(chunk);
-        },
-        close() {
-          res.end();
-        },
-      }));
+      resp.blob().then((blob) => {
+        blob.arrayBuffer().then((buf) => {
+          res.send(Buffer.from(buf))
+        })
+      });
+
     }
   })
 
